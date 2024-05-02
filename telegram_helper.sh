4c746432ac11d4bd9f13990ec0390dc506fdc2e7
Greendq <serj@dqteam.com>
2024-05-02 21:54:46 +0300

    Added support for multiple notification channels

--- telegram_helper.sh
+++ telegram_helper.sh
@@ -2,6 +2,12 @@
 SendTelegramMessage() {
     local message="${1}"
 	rawurlencode "${message}";
+
     local encodedMessage=${REPLY}
-/usr/bin/wget "https://api.telegram.org/bot${TG_API_TOKEN}/sendMessage?chat_id=${TG_CHAT_ID}&text=${encodedMessage}" -o /dev/null -O /dev/null
+    local arr=(${TG_CHAT_ID})
+
+    for chatId in ${arr[@]}; do
+        #echo Sending message to  ${chatId};
+        /usr/bin/wget "https://api.telegram.org/bot${TG_API_TOKEN}/sendMessage?chat_id=${chatId}&text=${encodedMessage}" -o /dev/null -O /dev/null
+    done
 }
